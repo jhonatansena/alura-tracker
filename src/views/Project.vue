057f88f5597f230d3f00f1d5249b88bc4b2e7a96
Jhonatan Sena <jcostasenna@hotmail.com>
2023-10-16 15:00:09 -0300

    refactor: remove unused semicolon

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -57,10 +57,10 @@ export default defineComponent({
             this.projectName=''
         },
         formatDate(date: Date): string {
-            const day = String(date.getDate()).padStart(2, '0');
-            const month = String(date.getMonth() + 1).padStart(2, '0');
-            const year = date.getFullYear();
-            return `${day}/${month}/${year}`;
+            const day = String(date.getDate()).padStart(2, '0')
+            const month = String(date.getMonth() + 1).padStart(2, '0')
+            const year = date.getFullYear()
+            return `${day}/${month}/${year}`
          }
     }
 })
