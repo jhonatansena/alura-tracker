fcc8ed250ff3a3ba261b7a49eafc70cccdeca3c9
Jhonatan Sena <jcostasenna@hotmail.com>
2023-10-18 23:18:54 -0300

    feat: use store in the form

--- src/components/Forms.vue
+++ src/components/Forms.vue
@@ -1,13 +1,27 @@
 <template>
     <div class="box forms-style">
         <div class="columns">
-            <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+            <div class="column is-5" role="form" aria-label="Formulário para criação de uma nova tarefa">
                 <input 
                     type="text" 
                     class="input"
                     placeholder="Qual tarefa você deseja iniciar?"
                     v-model="description"
                     >
+            </div>
+            <div class="column is-3">
+                <div class="select">
+                    <select v-model="idProject">
+                        <option value="">Selecione o projeto</option>
+                        <option 
+                         :value="project.id"
+                         v-for="project in projects"
+                         :key="project.id"
+                        >
+                        {{  project.name }}
+                    </option>
+                    </select>
+                </div>
             </div>
             <div class="column">
                 <PlaybackControls @toTimerFinished="finishTask"/>
@@ -18,13 +32,16 @@
 
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
 import PlaybackControls from './PlaybackControls.vue'
+import { useStore } from 'vuex'
+import  { key } from '@/store'
 
 export default defineComponent({
     data() {
         return {
-            description: ''
+            description: '',
+            idProject: '',
         }
     },
     // eslint-disable-next-line vue/multi-word-component-names
@@ -37,9 +54,16 @@ export default defineComponent({
         finishTask (timeElapsed: number): void {
                this.$emit('toSaveTask', {
                timeInSeconds: timeElapsed,
-               description: this.description
+               description: this.description,
+               project: this.projects.find(proj => proj.id === this.idProject)
             })
             this.description = ''
+        }
+    },
+    setup () {
+        const store = useStore(key)
+        return {
+            projects: computed(() => store.state.projects)
         }
     }
 })
