13cf39088821e9230fa9b84c8059968e32dcb62b
Jhonatan Sena <jcostasenna@hotmail.com>
2023-10-19 09:26:24 -0300

    refactor: remove fix initial state

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,36 +6,11 @@ interface State {
     projects: IProject[]
 }
 
-const formatDate = (date: Date): string => {
-    const day = String(date.getDate()).padStart(2, '0')
-    const month = String(date.getMonth() + 1).padStart(2, '0')
-    const year = date.getFullYear()
-    return `${day}/${month}/${year}`
- }
-
 export const key: InjectionKey<Store<State>> = Symbol()
 
 export const store = createStore<State>({
     state: {
-        projects: [
-            {
-                id: new Date().getTime().toString(),
-                name: 'Typescript',
-                startDate: formatDate(new Date())
-            },
-            {
-                id: new Date().getTime().toString(),
-                name: 'Vue',
-                startDate: formatDate(new Date())
-            },
-
-            {
-                id: new Date().getTime().toString(),
-                name: 'Vuex',
-                startDate: formatDate(new Date())
-            },
-
-        ]    
+        projects: [ ]    
     }
 })
 
