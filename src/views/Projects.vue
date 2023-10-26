09a98c09d97ea147a7c2b3dc8804d0c45ba347d1
Jhonatan Sena <jcostasenna@hotmail.com>
2023-10-26 20:47:58 -0300

    refactor: create projects component

---
+++ src/views/Projects.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="projects">
+        <h1 class="title">Projetos</h1>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Projects',
+})
+
+</script>
+
+<style>
+.projects {
+    padding: 1.25rem;
+}
+</style>
