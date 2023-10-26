81d9d64acb5faf0fddd3c9f99b768b643f955135
Jhonatan Sena <jcostasenna@hotmail.com>
2023-10-26 15:29:44 -0300

    refactor: remove forms part to another component

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,24 +1,20 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <section class="project">
-        <h1 class="title">Projeto</h1>
-        <form @submit.prevent="save">
-            <div class="field">
-                <label for="projectName" class="label">Nome do Projeto</label>
-                <input type="text" class="input" v-model="projectName" id="projectName">
-            </div>
-            <div class="field">
-                <button class="button" type="submit" style="background-color: darkblue; color: white; font-weight: bolder;">
-                    salvar
-                </button>
-            </div>
-        </form>
+        <h1 class="title">Projetos</h1>
+        <router-link to="/projects/new" class="button">
+            <span>
+                <i class="fas fa-plus"></i>
+            </span>
+            <span>Novo Projeto</span>
+        </router-link>
         <table class="table is-fullwidth">
             <thead>
                 <tr >
                     <th> ID </th>
                     <th> NOME </th>
                     <th>DATA DE INÍCIO</th>
+                    <th>Ações</th>
                 </tr>
             </thead>
             <tbody>
@@ -26,6 +22,15 @@
                     <td>{{  project.id }}</td>
                     <td>{{ project.name }}</td>
                     <td> {{ project.startDate }}</td>
+                    <td> 
+                        <router-link :to="`/projects/${project.id}`" class="button">
+                            <span class="icon is-small">
+                                <i class="fas fa-pencil-alt"></i>
+
+                            </span>
+
+                        </router-link>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -34,39 +39,14 @@
 
 <script lang="ts">
 import { computed, defineComponent } from 'vue'
-import IProject from '../interfaces/IProject'
 import { useStore } from '@/store'
 
 export default defineComponent({
     // eslint-disable-next-line vue/multi-word-component-names
     name: 'Project',
-    data() {
-        return {
-            projectName: ""
-        }
-    },
-    methods: {
-        save() {
-            const project: IProject={
-                id: new Date().getTime().toString(),
-                name: this.projectName,
-                startDate: this.formatDate(new Date())
-            }
-
-            this.store.commit('ADD_PROJECT', project)
-            this.projectName=''
-        },
-        formatDate(date: Date): string {
-            const day = String(date.getDate()).padStart(2, '0')
-            const month = String(date.getMonth() + 1).padStart(2, '0')
-            const year = date.getFullYear()
-            return `${day}/${month}/${year}`
-         }
-    },
     setup () {
         const store = useStore()
         return {
-            store,
             projects: computed(() => store.state.projects)
         }
     }
