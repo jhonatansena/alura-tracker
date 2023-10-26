7e31b7334c550f8998e8f6d583b52fc9d82614d5
Jhonatan Sena <jcostasenna@hotmail.com>
2023-10-26 20:47:32 -0300

    feat: create list component

---
+++ src/views/List.vue
@@ -0,0 +1,53 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <section>
+        <router-link to="/projects/new" class="button">
+            <span class="icon is-small">
+                 <i class="fas fa-plus"></i>
+            </span>
+            <span>Novo Projeto</span>
+        </router-link>
+        <table class="table is-fullwidth">
+            <thead>
+                <tr >
+                    <th> ID </th>
+                    <th> NOME </th>
+                    <th>DATA DE INÍCIO</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="project in projects" :key="project.id">
+                    <td>{{  project.id }}</td>
+                    <td>{{ project.name }}</td>
+                    <td> {{ project.startDate }}</td>
+                    <td> 
+                        <router-link :to="`/projects/${project.id}`" class="button">
+                            <span class="icon is-small">
+                                <i class="fas fa-pencil-alt"></i>
+
+                            </span>
+
+                        </router-link>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+import { useStore } from '@/store'
+
+export default defineComponent({
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'List',
+    setup () {
+        const store = useStore()
+        return {
+            projects: computed(() => store.state.projects)
+        }
+    }
+})
+</script>
