18229fd71fc643049a12557f08c87290c5372d22
Jhonatan Sena <jcostasenna@hotmail.com>
2023-12-29 15:04:00 -0300

    refactor(Form/Projects): use composition api instead option

--- src/views/Projects/Forms.vue
+++ src/views/Projects/Forms.vue
@@ -22,6 +22,8 @@ import { useStore } from '@/store'
 import { ADD_PROJECT_ACTION, UPDATE_PROJECT_ACTION } from '@/store/actionTypes'
 import { NotificationType } from '@/interfaces/INotification'
 import { notificationMixin } from '@/mixin/notify'
+import userNotification from '@/hooks/notify'
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
     // eslint-disable-next-line vue/multi-word-component-names
@@ -32,72 +34,77 @@ export default defineComponent({
         }
     },
     mixins: [notificationMixin],
-    methods: {
-        async save() {
-            if (this.id) {
-               try {
-                this.store.dispatch(UPDATE_PROJECT_ACTION, {
-                    id: this.id,
-                    name: this.projectName
-                })
-                this.handleWithSuccess('Projeto editado com sucesso!',
-                'Protinho, projeto já está editado!',
-                NotificationType.SUCCESS
-                )
-               } catch {
-                this.notify(
-            'Erro ao editar projeto',
-            'Houve um erro ao tentar editar o projeto. Por favor, tente novamente.',
-            NotificationType.DANGER
-        )
-               }
-            } else {
-                try {
-        const project: IProject = {
-            id: new Date().getTime().toString(),
-            name: this.projectName,
-            startDate: this.formatDate(new Date())
-        };
-
-        await this.store.dispatch(ADD_PROJECT_ACTION, project)
-        
-        this.handleWithSuccess(
-            'Novo projecto adicionado',
-            'Prontinho ;) seu projeto já está disponível',
-            NotificationType.SUCCESS
-        );
-    } catch {
-
-        this.notify(
-            'Erro ao adicionar projeto',
-            'Houve um erro ao tentar adicionar o projeto. Por favor, tente novamente.',
-            NotificationType.DANGER
-        )
-    }
-    }
-    },
-    handleWithSuccess(title: string, text: string, type: NotificationType) {
-        this.notify(title, text, type);
-
-        this.projectName = '';
-        this.$router.push('/projects');
-    },
-    formatDate(date: Date): string {
-        const day=String(date.getDate()).padStart(2, '0')
-        const month=String(date.getMonth()+1).padStart(2, '0')
-        const year=date.getFullYear()
-        return `${day}/${month}/${year}`
-    }
-    },
     setup(props) {
         const store = useStore()
+        const router = useRouter()
         const projectName = ref("")
-        if (props.id) {
-            const project = store.state.project.projects.find((project) => project.id === props.id)
-            projectName.value = project?.name ?? ''
+        const { notify } = userNotification()
+        const propID = props.id as string
+
+        if (propID) {
+            const project = store.state.project.projects.find((project) => project.id === propID)
+            projectName.value = project?.name??''
         }
+
+        const save = () => {
+            if (propID) {
+                try {
+                    store.dispatch(UPDATE_PROJECT_ACTION, {
+                        id: propID,
+                        name: projectName.value
+                    })
+                    handleWithSuccess('Projeto editado com sucesso!',
+                        'Protinho, projeto já está editado!',
+                        NotificationType.SUCCESS
+                    )
+                } catch {
+                    notify(
+                        'Erro ao editar projeto',
+                        'Houve um erro ao tentar editar o projeto. Por favor, tente novamente.',
+                        NotificationType.DANGER)
+                }
+            } else {
+                try {
+                    const project: IProject={
+                        id: new Date().getTime().toString(),
+                        name: projectName.value,
+                        startDate: formatDate(new Date())
+                    };
+
+                    store.dispatch(ADD_PROJECT_ACTION, project)
+
+                    handleWithSuccess(
+                        'Novo projecto adicionado',
+                        'Prontinho ;) seu projeto já está disponível',
+                        NotificationType.SUCCESS
+                    );
+                } catch {
+
+                    notify(
+                        'Erro ao adicionar projeto',
+                        'Houve um erro ao tentar adicionar o projeto. Por favor, tente novamente.',
+                        NotificationType.DANGER
+                    )
+                }
+            }
+        }
+
+        const handleWithSuccess = (title: string, text: string, type: NotificationType) => {
+            notify(title, text, type);
+
+            projectName.value = '';
+            router.push('/projects');
+        }
+
+        const formatDate = (date: Date): string => {
+            const day=String(date.getDate()).padStart(2, '0')
+            const month=String(date.getMonth()+1).padStart(2, '0')
+            const year=date.getFullYear()
+            return `${day}/${month}/${year}`
+        }
+
         return {
-            store,
+            save,
             projectName
         }
     }
