04271913518e5e3f9ffc50a1a87fc392c68499b0
Jhonatan Sena <jcostasenna@hotmail.com>
2024-08-15 09:53:32 -0300

    chore: commit test

--- src/views/List.vue
+++ src/views/List.vue
@@ -25,7 +25,6 @@
                         <router-link :to="`/projects/${project.id}`" class="button">
                             <span class="icon is-small">
                                 <i class="fas fa-pencil-alt"></i>
-
                             </span>
 
                         </router-link>
